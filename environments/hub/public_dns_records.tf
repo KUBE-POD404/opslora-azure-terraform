d1943973d9590e5e7adbccf87c8b387aa2633af0
Sowrabh0-0 <133626226+Sowrabh0-0@users.noreply.github.com>
2026-06-27 09:12:50 +0530

    infra: add opslora mail DNS records

--- environments/hub/public_dns_records.tf
+++ environments/hub/public_dns_records.tf
@@ -37,3 +37,52 @@ resource "azurerm_dns_a_record" "opslora_public" {
   records             = each.value.records
   tags                = var.tags
 }
+
+# Hostinger email is used by notification-service SMTP
+# (smtp.hostinger.com / notification@opslora.com). When opslora.com
+# nameservers moved from Hostinger to Azure DNS, mail authentication records
+# must also live in this Azure DNS zone.
+resource "azurerm_dns_mx_record" "opslora_mail" {
+  name                = "@"
+  zone_name           = azurerm_dns_zone.opslora.name
+  resource_group_name = azurerm_dns_zone.opslora.resource_group_name
+  ttl                 = 300
+
+  record {
+    preference = 5
+    exchange   = "mx1.hostinger.com"
+  }
+
+  record {
+    preference = 10
+    exchange   = "mx2.hostinger.com"
+  }
+
+  tags = var.tags
+}
+
+resource "azurerm_dns_txt_record" "opslora_spf" {
+  name                = "@"
+  zone_name           = azurerm_dns_zone.opslora.name
+  resource_group_name = azurerm_dns_zone.opslora.resource_group_name
+  ttl                 = 300
+
+  record {
+    value = "v=spf1 include:_spf.mail.hostinger.com ~all"
+  }
+
+  tags = var.tags
+}
+
+resource "azurerm_dns_txt_record" "opslora_dmarc" {
+  name                = "_dmarc"
+  zone_name           = azurerm_dns_zone.opslora.name
+  resource_group_name = azurerm_dns_zone.opslora.resource_group_name
+  ttl                 = 300
+
+  record {
+    value = "v=DMARC1; p=none; adkim=s; aspf=s"
+  }
+
+  tags = var.tags
+}
